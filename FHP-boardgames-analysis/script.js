1c4c1d433efa39404ed9ca4d2db9abf4a79178d8
Nadezda Kuzmina <gmnabl@gmail.com>
2019-06-29 16:19:29 +0200

    update
    text
    color
    pos sticky

--- FHP-boardgames-analysis/script.js
+++ FHP-boardgames-analysis/script.js
@@ -1,72 +1,50 @@
-var container = d3.select("#scrolly");
-var article = container.select("article");
-var characters = article.select(".category");
-var boxes = article.select(".authors");
+var scrolly = d3.select("#scrolly");
+/// var Characters
+var article = scrolly.selectAll("article");
+var figure = article.selectAll(".characters");
+
+/// var Autors
+var articleAuth = scrolly.select("#articleAuth");
+var boxes = articleAuth.select(".authors");
 var gameboxes = boxes.select(".game-boxes");
-var step = article.selectAll(".step");
+var step = scrolly.selectAll(".step");
 
-//game boxes//////////////////////////////////
+///SVG
 var boxesSize = Math.round((window.innerWidth - 200) / 12);
-
-// console.log(boxesSize);
-
-var svgBoxAll = d3
-  .select("#galleryAllSvg")
-  .append("svg")
-  .attr("height", "200px")
-  .attr("width", "1600px")
-  .append("g")
-  .attr("transform", "translate(0,0)");
-
-var svgBoxAuth = d3
-  .select("#gallerySvgAuthor")
-  .append("svg")
-  .attr("height", "100%")
-  .attr("width", "100%")
-  .append("g")
-  .attr("transform", "translate(0,0)");
-
-var svgBoxIll = d3
-  .select("#gallerySvgIllustrators")
-  .append("svg")
-  .attr("height", "100%")
-  .attr("width", "100%")
-  .append("g")
-  .attr("transform", "translate(0,0)");
 
 // var defs = svgBox.append("defs");
 
 ////?????????////////
 
-var currentValue = 0,
-  duration = 3000,
-  newValue = 5;
+// var currentValue = 0,
+//   duration = 3000,
+//   newValue = 5;
 
-var svgPercentage = d3
-  .select("#svgPercentage")
-  .append("svg")
-  .attr("width", 200)
-  .attr("height", 100);
+// var svgPercentage = d3
+//   .select("#svgPercentage")
+//   .append("svg")
+//   .attr("width", 200)
+//   .attr("height", 100);
 
-var numberIncrease = function() {
-  svgPercentage
-    .append("text", "%")
-    .attr("x", "0")
-    .attr("y", "0")
-    .transition()
-    .duration(3500);
-  //   .tween("text", function() {
-  //     var i = d3.interpolate(currentValue, newValue);
-  //     return function(t) {
-  //         this.textContent = Math.round(i(t));
-  //     };
-  // });​
-};
+// var numberIncrease = function() {
+//   svgPercentage
+//     // .append("text", "%")
+//     .attr("x", "0")
+//     .attr("y", "0")
+//     .transition()
+//     .duration(3500);
+//   // .tween("text", function() {
+//   //   var i = d3.interpolate(currentValue, newValue);
+//   //   return function(t) {
+//   //     this.textContent = Math.round(i(t));
+//   //   };
+//   // });
+// };
 
 var tooltip = d3.select("#tooltipAuth");
 
 var svgChar = d3
-  .select(".SvgCharClass")
+  .selectAll(".SvgCharClass")
   .append("svg")
   .attr("height", "500px")
   .attr("width", "1000px")
@@ -101,6 +79,30 @@ var svgCharNoneB = d3
   .append("g")
   .attr("transform", "translate(0,0)");
 
+// var svgBoxAll = d3
+//   .selectAll(".game-boxes")
+//   .append("svg")
+//   .attr("height", "500px")
+//   .attr("width", "1000px")
+//   .append("g")
+//   .attr("transform", "translate(0,0)");
+
+var svgBoxAuth = d3
+  .select("#gallerySvgAuthor")
+  .append("svg")
+  .attr("height", "500px")
+  .attr("width", "1000px")
+  .append("g")
+  .attr("transform", "translate(0,0)");
+
+var svgBoxIll = d3
+  .select("#gallerySvgIllustrators")
+  .append("svg")
+  .attr("height", "500px")
+  .attr("width", "1000px")
+  .append("g")
+  .attr("transform", "translate(0,0)");
+
 d3.csv("test.csv", function(err, data) {
   if (err) {
     console.log(err);
@@ -124,7 +126,7 @@ d3.csv("test.csv", function(err, data) {
     // console.log(d);
   });
 
-  function svgCharMake() {
+  var svgCharMake = function() {
     svgChar
       .selectAll(".char-in-rect")
       .data(data)
@@ -143,11 +145,11 @@ d3.csv("test.csv", function(err, data) {
       .attr("height", "75px")
       .style("fill-opacity", 1)
       .attr("border-radius", "30")
-      .attr("fill", "green");
-  }
+      .attr("fill", "#0E28EE");
+  };
 
   var svgCharNoneMen = function() {
-    svgChar.selectAll(".char-in-rect").style("fill-opacity", 0);
+    // svgChar.selectAll(".char-in-rect").style("fill-opacity", 0);
 
     svgCharNoneM
       .selectAll(".char-in-rect")
@@ -165,13 +167,13 @@ d3.csv("test.csv", function(err, data) {
       .attr("class", "char-in-rect")
       .attr("width", "50")
       .attr("height", "75px")
-      .style("fill-opacity", 0.75)
+      .style("fill-opacity", 1)
       .attr("border-radius", "30")
       .attr("fill", function(d) {
         if (d.Nicht_männliche_C === "nein") {
-          return "red";
+          return "#a57c1b";
         } else {
-          return "green";
+          return "#0E28EE";
         }
       });
   };
@@ -193,15 +195,15 @@ d3.csv("test.csv", function(err, data) {
       .attr("class", "char-in-rect")
       .attr("width", "50")
       .attr("height", "75px")
-      .style("fill-opacity", 0.75)
+      .style("fill-opacity", 1)
       .attr("border-radius", "30")
       .attr("fill", function(d) {
         if (d.Nichtweiße_C === "nein") {
-          return "red";
+          return "#a57c1b";
         } else if (d.Nichtweiße_C === "x") {
           return "grey";
         } else {
-          return "green";
+          return "#0E28EE";
         }
       });
   };
@@ -210,6 +212,7 @@ d3.csv("test.csv", function(err, data) {
     svgCharNoneB
       .selectAll(".char-in-rect")
       .data(data)
+
       .enter()
       .append("rect")
       .attr("rx", 6)
@@ -223,20 +226,47 @@ d3.csv("test.csv", function(err, data) {
       .attr("class", "char-in-rect")
       .attr("width", "50")
       .attr("height", "75px")
-      .style("fill-opacity", 0.75)
+      .style("fill-opacity", 1)
       .attr("border-radius", "30")
+      .sort()
       .attr("fill", function(d) {
         if (d.Nicht_normschön === "nein") {
-          return "red";
+          return "#a57c1b";
         } else if (d.Nicht_normschön === "x") {
           return "grey";
         } else {
-          return "green";
+          return "#0E28EE";
         }
       });
   };
 
-  var boxesRender = function() {
+  var boxesRenderAll = function() {
+    svgBoxAll
+      .selectAll(".game-in-rect")
+      .data(data)
+      .enter()
+      .append("image")
+      .attr("xlink:href", function(d) {
+        return d.image_path;
+      })
+      .attr("class", "game-in-rect")
+      .style("fill-opacity", 1)
+      .attr("x", function(d) {
+        return d.xcoordinate;
+      })
+      .attr("y", function(d) {
+        return d.ycoordinate;
+      })
+      .attr("width", boxesSize)
+      .attr("height", boxesSize)
+      // .on("click", function(d) {})
+      .on("mouseover", handleMouseOver)
+      .on("mouseout", handleMouseOut);
+  };
+
+  var boxesRenderAuth = function() {
+    // svgBoxAll.selectAll(".game-in-rect").style("fill-opacity", 0);
+
     svgBoxAuth
       .selectAll(".game-in-rect")
       .data(data)
@@ -254,12 +284,23 @@ d3.csv("test.csv", function(err, data) {
       })
       .attr("width", boxesSize)
       .attr("height", boxesSize)
+      .attr("fill", function(d) {
+        if (d.Nicht_normschön === "nein") {
+          return "#a57c1b";
+        } else if (d.Nicht_normschön === "x") {
+          return "grey";
+        } else {
+          return "#0E28EE";
+        }
+      })
       // .on("click", function(d) {})
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut);
   };
 
   var boxesRenderIll = function() {
+    // svgBoxAll.selectAll(".game-in-rect").style("fill-opacity", 0);
+
     svgBoxIll
       .selectAll(".game-in-rect")
       .data(data)
@@ -277,6 +318,15 @@ d3.csv("test.csv", function(err, data) {
       })
       .attr("width", boxesSize)
       .attr("height", boxesSize)
+      .attr("fill", function(d) {
+        if (d.Nicht_normschön === "nein") {
+          return "#a57c1b";
+        } else if (d.Nicht_normschön === "x") {
+          return "grey";
+        } else {
+          return "#0E28EE";
+        }
+      })
       // .on("click", function(d) {})
       .on("mouseover", handleMouseOver)
       .on("mouseout", handleMouseOut);
@@ -293,37 +343,63 @@ d3.csv("test.csv", function(err, data) {
   }
 
   svgCharMake();
-  boxesRender();
+  // boxesRenderAll();
   // boxesAllRender();
 
   //scroller////////////////////////////////////////////////////
+  // var main = d3.select('main')
+  // var scrolly = main.select('#scrolly');
+  // var figure = scrolly.select('figure');
+  // var article = scrolly.select('article');
+  // var step = article.selectAll('.step');
+
   const scroller = scrollama();
 
   var activateFunctions = [];
-  activateFunctions[0] = numberIncrease;
+  activateFunctions[0] = svgCharNoneMen;
   activateFunctions[1] = svgCharNoneWhite;
-  activateFunctions[2] = svgCharNoneMen;
-  activateFunctions[3] = svgCharNoneBeautiful;
-  activateFunctions[4] = boxesRender;
-  activateFunctions[5] = boxesRenderIll;
+  activateFunctions[2] = svgCharNoneBeautiful;
+  activateFunctions[3] = boxesRenderAuth;
+  activateFunctions[4] = boxesRenderIll;
+  // activateFunctions[5] = boxesRenderIll;
+
+  function handleResize() {
+    // 1. update height of step elements
+    var stepH = Math.floor(window.innerHeight * 0.75);
+    step.style("height", stepH + "px");
+    var figureHeight = window.innerHeight / 2;
+    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+    figure
+      .style("height", figureHeight + "px")
+      .style("top", figureMarginTop + "px");
+    // 3. tell scrollama to update new element dimensions
+    scroller.resize();
+  }
+  // scrollama event handlers
 
   function handleStepEnter(response) {
     console.log(response.index, "-------- enter");
+    // response = { element, direction, index }
+    // add color to current step only
     step.classed("is-active", function(d, i) {
       return i === response.index;
     });
-
-    container.select("div").text(response.index + 1);
-    container.call(activateFunctions[response.index]);
+    // update graphic based on step
+    // figure.select("p").text(response.index);
+    scrolly.call(activateFunctions[response.index]);
   }
-
+  function setupStickyfill() {
+    d3.selectAll(".sticky").each(function() {
+      Stickyfill.add(this);
+    });
+  }
   function handleStepProgress(response) {
     var el = d3.select(response.element);
 
-    var val = el.attr("data-step");
-    var rgba = "rgba(" + val + ", " + response.progress + ")";
-    el.style("background-color", rgba);
-    el.select(".progress").text(d3.format(".1%")(response.progress));
+    // var val = el.attr("data-step");
+    // var rgba = "rgba(" + val + ", " + response.progress + ")";
+    // el.style("background-color", rgba);
+    // el.select(".progress").text(d3.format(".1%")(response.progress));
 
     console.log(response);
   }
@@ -332,17 +408,21 @@ d3.csv("test.csv", function(err, data) {
     console.log(response.index, "-------- exit");
     response.element.classList.remove("is-active");
   }
-
   function init() {
+    setupStickyfill();
+    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+    handleResize();
+    // 2. setup the scroller passing options
+    // 		this will also initialize trigger observations
+    // 3. bind scrollama event handlers (this can be chained like below)
     scroller
       .setup({
-        container: "#scrolly",
+        scrolly: "#scrolly",
         article: "article",
-        percent: ".zero",
         characters: ".category",
         boxes: ".authors",
-        step: "#scrolly article .step",
-        offset: 0.2,
+        step: "#scrolly .step",
+        offset: 0.8,
         debug: true
       })
       .onStepEnter(handleStepEnter)
@@ -351,9 +431,56 @@ d3.csv("test.csv", function(err, data) {
 
     window.addEventListener("resize", scroller.resize);
   }
+  // kick things off
   init();
 });
-/////// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
+//   function handleStepEnter(response) {
+//     console.log(response.index, "-------- enter");
+//     step.classed("is-active", function(d, i) {
+//       return i === response.index;
+//     });
+
+//     scrolly.select("div").text(response.index + 1);
+//     scrolly.call(activateFunctions[response.index]);
+//   }
+
+//   function handleStepProgress(response) {
+//     var el = d3.select(response.element);
+
+//     // var val = el.attr("data-step");
+//     // var rgba = "rgba(" + val + ", " + response.progress + ")";
+//     // el.style("background-color", rgba);
+//     // el.select(".progress").text(d3.format(".1%")(response.progress));
+
+//     console.log(response);
+//   }
+
+//   function handleStepExit(response) {
+//     console.log(response.index, "-------- exit");
+//     response.element.classList.remove("is-active");
+//   }
+
+//   function init() {
+//     scroller
+//       .setup({
+//         scrolly: "#scrolly",
+//         article: "#article",
+//         characters: ".category",
+//         boxes: ".authors",
+//         step: "#scrolly article .step",
+//         offset: 0.2,
+//         debug: true
+//       })
+//       .onStepEnter(handleStepEnter)
+//       .onStepExit(handleStepExit)
+//       .onStepProgress(handleStepProgress);
+
+//     window.addEventListener("resize", scroller.resize);
+//   }
+//   init();
+// });
+// /////// https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
 
 var options = {
   root: document.querySelector("#scrolly"),
@@ -363,5 +490,5 @@ var options = {
 
 // var observer = new IntersectionObserver(callback, options);
 
-// var target = document.querySelector('.step');
+// var target = document.querySelector(".step");
 // observer.observe(target);
